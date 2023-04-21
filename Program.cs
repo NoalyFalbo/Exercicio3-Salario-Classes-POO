146e9bd480979b05d3e0ef388b65816dd39874a6
Noaly Falbo <noalyfalbos@outlook.com>
2023-04-21 10:31:50 -0300

    primeira versão codigo funcionario-salario

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace exercicioClasses3
+{
+
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+
+      Console.Clear();
+
+      Funcionario pessoa1 = new Funcionario();
+      Funcionario pessoa2 = new Funcionario();
+
+      Console.WriteLine("Digite o nome do primeiro funcionario:");
+      pessoa1.Nome = Console.ReadLine();
+      Console.WriteLine("Digite o salário do primeiro funcioanario:");
+      double.TryParse(Console.ReadLine(), out pessoa1.salario);
+      Console.WriteLine("Digite o nome do segundo funcionario:");
+      pessoa2.Nome = Console.ReadLine();
+      Console.WriteLine("Digite o salário do segundo funcioanario:");
+      double.TryParse(Console.ReadLine(), out pessoa2.salario);
+
+    }
+  }
+}
